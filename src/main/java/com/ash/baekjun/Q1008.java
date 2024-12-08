07e7c54550b766f7319eb16260bcfb50cbb43a95
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2024-12-08 15:36:07 +0900

    init

--- src/main/java/com/ash/baekjun/Q1008.java
+++ src/main/java/com/ash/baekjun/Q1008.java
@@ -1,4 +1,16 @@
 package com.ash.baekjun;
 
+import java.util.Scanner;
+
 public class Q1008 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        String[] line = sc.nextLine().split(" ");
+        Double d = Double.parseDouble(line[0]) / Double.parseDouble(line[1]);
+        System.out.print(d);
+    }
+
 }
--- src/main/java/com/ash/baekjun/Q2738.java
+++ src/main/java/com/ash/baekjun/Q2738.java
@@ -1,4 +1,41 @@
 package com.ash.baekjun;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
 public class Q2738 {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        List<Integer> size = Arrays.stream(scanner.nextLine().split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+        int n = size.get(0); // row
+        int m = size.get(1); // column
+
+        int[][] result = new int[n][m];
+
+        int cnt = 0; // 0,1,2,3,4,5
+
+        while (scanner.hasNext()) {
+            int nIdx = cnt++ % n;
+            String line = scanner.nextLine();
+            List<Integer> nums = Arrays.stream(line.split(" ")).map(Integer::parseInt).collect(Collectors.toList());
+
+            for (int i = 0; i < m; i++) {
+                result[nIdx][i] += nums.get(i);
+            }
+        }
+
+        scanner.close();
+
+        for (int[] ints : result) {
+            StringBuilder t = new StringBuilder();
+            for (int anInt : ints) {
+                t.append(anInt).append(" ");
+            }
+            System.out.println(t);
+        }
+    }
 }
