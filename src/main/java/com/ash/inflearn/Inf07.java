248e096635df30980962e6459e5a44cbcc96c4c3
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2024-12-12 17:09:42 +0900

    인프런 강의 07 - 회문문자열

---
+++ src/main/java/com/ash/inflearn/Inf07.java
@@ -0,0 +1,28 @@
+package com.ash.inflearn;
+
+import java.util.Scanner;
+
+public class Inf07 {
+
+    public String solution(String text) {
+        int length = text.length();
+        text = text.toUpperCase();
+
+        for (int i =0; i < length / 2; i++) {
+            if (text.charAt(i) != text.charAt(length - i - 1)) {
+                return "NO";
+            }
+        }
+
+        return "YES";
+    }
+
+
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        Inf07 inf07 = new Inf07();
+        System.out.println(inf07.solution(in.nextLine()));
+        return ;
+    }
+
+}
---
+++ src/test/java/com/ash/inflearn/Inf07Test.java
@@ -0,0 +1,40 @@
+package com.ash.inflearn;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Inf07Test {
+
+    @Test
+    public void yes() {
+
+        Inf07 solution = new Inf07();
+
+        String result = solution.solution("gooG");
+
+        assertEquals("YES", result);
+    }
+
+    @Test
+    public void yes2() {
+
+        Inf07 solution = new Inf07();
+
+        String result = solution.solution("gofoG");
+
+        assertEquals("YES", result);
+    }
+
+    @Test
+    public void no() {
+
+        Inf07 solution = new Inf07();
+
+        String result = solution.solution("goofG");
+
+        assertEquals("NO", result);
+    }
+
+
+}
