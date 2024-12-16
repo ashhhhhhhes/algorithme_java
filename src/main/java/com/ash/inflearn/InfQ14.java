38e72553dd31a1ab74c1197e01dffc7aa1f11bbf
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2024-12-16 14:41:16 +0900

    인프런 강의 (배열) 2. 보이는 학생 (배열 선언 수정)

--- src/main/java/com/ash/inflearn/InfQ14.java
+++ src/main/java/com/ash/inflearn/InfQ14.java
@@ -5,10 +5,6 @@ import java.util.Arrays;
 public class InfQ14 {
 
     public String solution(int N, int[] K) {
-
-        K = Arrays.copyOf(K, N + 1);
-        K[N] = 0;
-
         int count = 1;
         int temp = K[0];
         for (int i = 1; i < N; i++) {
