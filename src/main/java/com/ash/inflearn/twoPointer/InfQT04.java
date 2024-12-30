e156fb1f5de67f1d9d9b9925479d7c5d1476bdc3
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2024-12-30 15:44:55 +0900

    인프런 강의 (two pointers) 4. 연속 부분수열 (정답 코드 추가)

--- src/main/java/com/ash/inflearn/twoPointer/InfQT04.java
+++ src/main/java/com/ash/inflearn/twoPointer/InfQT04.java
@@ -8,22 +8,37 @@ public class InfQT04 {
     // N, M
     // 합이 6이 되는 연속부분수열은 {2, 1, 3}, {1, 3, 1, 1}, {3, 1, 1, 1}
     public int solution(int n, int m, int[] list) {
-            int result = 0;
+        int result = 0;
 
-            int lt = 0, rt = 0;
-            int sum = list[0];
+        int lt = 0, rt = 0;
+        int sum = list[0];
 
-            while (lt < n && rt < n) {
-                if (sum == m) {
-                    result++;
-                    sum -= list[lt++];
-                } else if (sum > m) {
-                    sum -= list[lt++];
-                } else {
-                    if (++rt >= n) break;
-                    sum += list[rt];
-                }
+        while (rt < n) {
+            if (sum == m) {
+                result++;
+                sum -= list[lt++];
+            } else if (sum > m) {
+                sum -= list[lt++];
+            } else {
+                if (++rt >= n) break;
+                sum += list[rt];
             }
+        }
+
+        return result;
+    }
+
+    public int correctAnswer(int n, int m, int[] list) {
+        int sum = 0, lt = 0, result = 0;
+
+        for (int rt = 0; rt < n; rt++) {
+            sum += list[rt];
+            if (sum == m) result++;
+            while (sum >= m) {
+                sum -= list[lt++];
+                if (sum == m) result++;
+            }
+        }
 
         return result;
     }
--- src/test/java/com/ash/inflearn/twoPointer/InfQT04Test.java
+++ src/test/java/com/ash/inflearn/twoPointer/InfQT04Test.java
@@ -10,7 +10,7 @@ class InfQT04Test {
     public void test() {
         InfQT04 infQT04 = new InfQT04();
 
-        assertEquals(3, infQT04.solution(8, 6, new int[]{
+        assertEquals(3, infQT04.correctAnswer(8, 6, new int[]{
                 1, 2, 1, 3, 1, 1, 1, 2
         }));
     }
