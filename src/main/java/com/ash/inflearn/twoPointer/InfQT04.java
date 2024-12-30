8f29efce92db41e84e95735ce03ffeb5e9c214ce
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2024-12-30 15:36:27 +0900

    인프런 강의 (two pointers) 4. 연속 부분수열 (투포인트로 수정)

--- src/main/java/com/ash/inflearn/twoPointer/InfQT04.java
+++ src/main/java/com/ash/inflearn/twoPointer/InfQT04.java
@@ -8,20 +8,22 @@ public class InfQT04 {
     // N, M
     // 합이 6이 되는 연속부분수열은 {2, 1, 3}, {1, 3, 1, 1}, {3, 1, 1, 1}
     public int solution(int n, int m, int[] list) {
-        int  result = 0;
+            int result = 0;
 
-        for (int i = 0; i < n; i++) {
-            int sum = 0;
-            for (int k = i; k < n; k++) {
-                sum += list[k];
+            int lt = 0, rt = 0;
+            int sum = list[0];
+
+            while (lt < n && rt < n) {
                 if (sum == m) {
                     result++;
-                    break;
+                    sum -= list[lt++];
                 } else if (sum > m) {
-                    break;
+                    sum -= list[lt++];
+                } else {
+                    if (++rt >= n) break;
+                    sum += list[rt];
                 }
             }
-        }
 
         return result;
     }
