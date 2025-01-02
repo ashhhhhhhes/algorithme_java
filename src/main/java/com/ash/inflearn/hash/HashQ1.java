a3b26e501d50328bfd17e9d9a9666a8271cb0afc
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2025-01-03 08:46:09 +0900

    인프런 강의 (HashMap, TreeSet) 6. 1. 학급 회장(해쉬) 코드 수정.

--- src/main/java/com/ash/inflearn/hash/HashQ1.java
+++ src/main/java/com/ash/inflearn/hash/HashQ1.java
@@ -9,27 +9,22 @@ public class HashQ1 {
 
     public String solution(int n, String str) {
 
-        HashMap<String, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> map = new HashMap<>();
 
         for (char c : str.toCharArray()) {
-            String key = c + "";
-            if (map.containsKey(key)) {
-                map.put(key, map.get(key) + 1);
-            } else {
-                map.put(key, 1);
-            }
+            map.put(c, map.getOrDefault(c, 0) + 1);
         }
 
         int max = 0;
-        String result = "";
-        for (String key : map.keySet()) {
+        char result = ' ';
+        for (char key : map.keySet()) {
             if (max < map.get(key)) {
                 max = map.get(key);
                 result = key;
             }
         }
 
-        return result;
+        return Character.toString(result);
     }
 
 }
