1be488e39b6d0203e2002e9b2758656304731b61
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2025-01-06 12:24:08 +0900

    인프런 강의 (HashMap, TreeSet) 4. 모든 아나그램 찾기(Hash, slide window) - Map.equals 사용한 방식으로 재풀이.

--- src/main/java/com/ash/inflearn/hash/HashQ4.java
+++ src/main/java/com/ash/inflearn/hash/HashQ4.java
@@ -38,4 +38,29 @@ public class HashQ4 {
         return result;
     }
 
+    public Integer solution2(String S, String T) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (Character c : T.toCharArray()) {
+            map.put(c, 0);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < T.length() - 1; i++) {
+            map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0) + 1);
+        }
+
+        int pl = 0, result = 0;
+        for (int i = T.length() - 1; i < S.length(); i++) {
+            map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0) + 1);
+            if (tMap.equals(map)) result++;
+            if (map.containsKey(S.charAt(pl))) map.put(S.charAt(pl), map.get(S.charAt(pl)) - 1);
+            if (map.getOrDefault(S.charAt(pl) , 0) == 0) map.remove(S.charAt(pl));
+            pl++;
+        }
+
+        return result;
+    }
+
 }
