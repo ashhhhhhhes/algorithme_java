580f844f0fa8e31f7519082e0269fa75374994be
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2025-01-06 12:15:47 +0900

    인프런 강의 (HashMap, TreeSet) 4. 모든 아나그램 찾기(Hash, slide window)

---
+++ src/main/java/com/ash/inflearn/hash/HashQ4.java
@@ -0,0 +1,41 @@
+package com.ash.inflearn.hash;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+// Hash - 4. 모든 아나그램 찾기
+public class HashQ4 {
+
+    public Integer solution(String S, String T) {
+
+        Map<Character, Integer> map = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for (Character c : T.toCharArray()) {
+            map.put(c, 0);
+            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
+        }
+        for (int i = 0; i < T.length() - 1; i++) {
+            if (map.containsKey(S.charAt(i))) map.put(S.charAt(i), map.get(S.charAt(i)) + 1);
+        }
+
+        int pl = 0, result = 0;
+        for (int i = T.length() - 1; i < S.length(); i++) {
+            if (map.containsKey(S.charAt(i))) map.put(S.charAt(i), map.getOrDefault(S.charAt(i), 0) + 1);
+            boolean flag = true;
+            for (Character key : map.keySet()) {
+                if (!Objects.equals(map.get(key), tMap.get(key))) {
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag) result++;
+            if (map.containsKey(S.charAt(pl))) map.put(S.charAt(pl), map.get(S.charAt(pl)) - 1);
+            pl++;
+        }
+
+        return result;
+    }
+
+}
