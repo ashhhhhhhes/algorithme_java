680db35604f356d96e25afd0baaf19958a818968
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2025-01-15 12:15:42 +0900

    인프런 강의 (Sorting and Searching) 1. 선택 정렬 (인덱스 방식)

--- src/main/java/com/ash/inflearn/sort/SelectionSort.java
+++ src/main/java/com/ash/inflearn/sort/SelectionSort.java
@@ -24,4 +24,21 @@ public class SelectionSort {
                 .collect(Collectors.joining(" "));
     }
 
+    public String solution2(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int idx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[idx] > arr[j]) {
+                    idx = j;
+                }
+            }
+            int tmp = arr[idx];
+            arr[idx] = arr[i];
+            arr[i] = tmp;
+        }
+
+        return Arrays.stream(arr).mapToObj(String::valueOf)
+                .collect(Collectors.joining(" "));
+    }
+
 }
--- src/test/java/com/ash/inflearn/sort/SelectionSortTest.java
+++ src/test/java/com/ash/inflearn/sort/SelectionSortTest.java
@@ -9,6 +9,6 @@ class SelectionSortTest {
     @Test
     void solution() {
         SelectionSort T = new SelectionSort();
-        assertEquals("5 7 11 13 15 23", T.solution(new int[]{13, 5, 11, 7, 23, 15}));
+        assertEquals("5 7 11 13 15 23", T.solution2(new int[]{13, 5, 11, 7, 23, 15}));
     }
 }
