c52d9a02cbc6d90a9be550b5dad6f7d899af1f02
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2025-02-12 09:34:41 +0900

    인프런 강의 (Sorting and Searching) 8. 이분검색

---
+++ src/main/java/com/ash/inflearn/sort/SearchQ8.java
@@ -0,0 +1,25 @@
+package com.ash.inflearn.sort;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Sorting and Searching 8. 이분검색
+ */
+public class SearchQ8 {
+
+    public int solution(int n, int t, List<Integer> arr) {
+        Collections.sort(arr);
+
+        int low = 0, high = n - 1;
+
+        for (int i = 0; i < arr.size(); i++) {
+            int mid = (low + high) / 2;
+            if (arr.get(mid) < t) low = mid;
+            else if (arr.get(mid) > t) high = mid;
+            else return mid + 1;
+        }
+
+        return 0;
+    }
+}
---
+++ src/test/java/com/ash/inflearn/sort/SearchQ8Test.java
@@ -0,0 +1,29 @@
+package com.ash.inflearn.sort;
+
+import com.ash.utils.TestScanner;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SearchQ8Test {
+
+    @Test
+    public void test() {
+        TestScanner testScanner = new TestScanner("8 32\n" +
+                "23 87 65 12 57 32 99 81\n");
+        Scanner sc = testScanner.getScanner();
+        int n = sc.nextInt();
+        int t = sc.nextInt();
+        List<Integer> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            arr.add(sc.nextInt());
+        }
+        SearchQ8 T = new SearchQ8();
+        assertEquals(3, T.solution(n, t, arr));
+    }
+
+}
