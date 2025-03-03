353c135c75486e97d3f043c101a5d50a9b74ab4e
HONG YOON PYO <94228503+ashhhhhhhes@users.noreply.github.com>
2025-03-03 18:09:07 +0900

    인프런 강의 (Sorting and Searching) 피보나치 수열

--- src/main/java/com/ash/inflearn/recursive/RecursiveQ4.java
+++ src/main/java/com/ash/inflearn/recursive/RecursiveQ4.java
@@ -4,12 +4,11 @@ public class RecursiveQ4 {
     int[] arr = null;
 
     public int fibonacci(int n) {
-        if (arr == null) {
-            arr = new int[n + 1];
-        }
+        if (arr == null) arr = new int[n + 1];
+        if (arr[n] > 0) return arr[n];
         if (n == 1) return arr[n] = 1;
         else if (n == 2) return arr[n] = 1;
-        else return arr[n] = this.fibonacci(n - 2) + this.fibonacci(n - 1);
+        else return arr[n] = fibonacci(n - 2) + fibonacci(n - 1);
     }
 
     public String solution(int n) {
